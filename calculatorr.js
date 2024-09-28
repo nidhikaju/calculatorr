ecd0ca72f2fd30b9f3c2ef9fa851d4c00b9f3b01
nidhikaju <161727726+nidhikaju@users.noreply.github.com>
2024-09-28 11:45:21 +0530

    Add files via upload

---
+++ calculatorr.js
@@ -0,0 +1,17 @@
+let string = "";
+let buutons = document.querySelectorAll('.button');
+Array.from(buutons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML == 'C') {
+            string = ""
+            document.querySelector('input').value = string;
+        } else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
